a7ea5851bcb57a3e43ebe377d226d396300f4b87
Brr1-99 <93494400+Brr1-99@users.noreply.github.com>
2021-11-16 22:34:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -369,8 +369,39 @@ def db_for_index(db):
         ctable = list(cursor1.fetchall())
         for table in ctable:
             tables.append(table[0])
-    else:
-        base = 2
+
+    elif db == 'manten_correctivo':
+
+        page = request.args.get(get_page_parameter(), type=int, default=1)
+        limit = 5
+        offset = page * limit - limit
+
+        cursor2.execute('Select * FROM incidencias ORDER BY `id` DESC LIMIT %s OFFSET %s', (limit, offset))
+        d1 = cursor2.fetchall()
+        t1 = len(d1)
+
+        cursor2.execute('Show Columns FROM incidencias')
+        c1 = cursor2.fetchall()
+        datos.append([d1, c1, 'incidencias'])
+
+        cursor2.execute('Select * FROM protocolos ORDER BY `id` DESC LIMIT %s OFFSET %s', (limit, offset))
+        d2 = cursor2.fetchall()
+        t2 = len(d2)
+
+        cursor2.execute('Show Columns FROM protocolos')
+        c2 = cursor2.fetchall()
+        datos.append([d2, c2, 'protocolos'])
+
+        pagination = Pagination(page=page, per_page=limit, total=t1, record_name='index')
+        pagination2 = Pagination(page=page, per_page=limit, total=t2, record_name='index')
+        pages.append(pagination)
+        pages.append(pagination2)
+
+        cursor1.execute("""SELECT TABLE_NAME from information_schema.tables where table_schema = '{0}'""".format(db))
+        ctable = list(cursor1.fetchall())
+        for table in ctable:
+            tables.append(table[0])
+
     return datos, db, pages, tables
 
 
@@ -379,6 +410,9 @@ def db_for_columns(db, table):
     if db == 'inventario':
         cursor1.execute('Show Columns FROM {0}'.format(table))
         col = cursor1.fetchall()
+    elif db == 'manten_correctivo':
+        cursor2.execute('Show Columns FROM {0}'.format(table))
+        col = cursor2.fetchall()
     return col
 
 
@@ -388,6 +422,9 @@ def db_cursor(db):
     if db == 'inventario':
         cursor = cursor1
         mydb = mydb1
+    elif db == 'manten_correctivo':
+        cursor = cursor2
+        mydb = mydb2
     return cursor, mydb
 
 
