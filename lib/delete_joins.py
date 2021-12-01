c77b017dff92b305a73acc3d58ae3204ee412467
Brr1-99 <93494400+Brr1-99@users.noreply.github.com>
2021-12-01 19:43:10 +0100

    delete joins

---
+++ lib/delete_joins.py
@@ -0,0 +1,10 @@
+from config.mydb1 import db1
+
+
+def delete_joins(id_maquina, id_comp):
+    db, cursor1 = db1()
+    cursor1.execute("DELETE FROM `componente_maquina` WHERE `id_componente`= '{0}' AND `id_maquina` = '{1}'".format(id_comp, id_maquina))
+    message = f'Relación eliminada entre la máquina {id_maquina} y la pieza con id {id_comp}'
+    db.commit()
+    cursor1.close()
+    return message
--- relacionar/join.py
+++ relacionar/join.py
@@ -1,16 +1,23 @@
-from flask import render_template, Blueprint, flash, redirect, url_for
+from flask import render_template, Blueprint, flash, redirect, url_for, request
 from lib.comprobar_sesión import comprobar_sesion
 from lib.table_for_joints import table_joints
+from lib.delete_joins import delete_joins
 
 joint = Blueprint('bp_join', __name__, static_folder="static", template_folder="templates_join")
 
 
-@joint.route('/<string:db>/<string:table>/<string:id>')
-def join(db, table, id):
+@joint.route('/<string:db>/<string:table>/<string:id>/<int:option>')
+def join(db, table, id, option):
     login = comprobar_sesion()[0]
     if login:
-        message, join1 = table_joints(db, table, id)
-        flash(f"{message} {join1}")
-        return redirect(url_for('bp_index.index', db=db))
+        if option == 0:
+            message, join1 = table_joints(db, table, id)
+            flash(f"{message} {join1}")
+            return redirect(url_for('bp_index.index', db=db))
+        else:
+            last_id = request.referrer.split('/')[-1]
+            message_disjoin = delete_joins(last_id, id)
+            flash(f"{message_disjoin}")
+            return redirect(url_for('bp_index.index', db=db))
     else:
         return render_template('ingresar.html')
