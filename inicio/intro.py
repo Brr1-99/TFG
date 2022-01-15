ff5c8ef217d938368640dd58a06c7952ba01791f
Brr1-99 <93494400+Brr1-99@users.noreply.github.com>
2022-01-15 10:07:34 +0100

    cards.py formateadas

--- inicio/intro.py
+++ inicio/intro.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 from lib.comprobar_sesión import comprobar_sesion
+from lib.cards import cards
 
 iniciar = Blueprint('bp_inicio', __name__, static_folder="static", template_folder="templates_intro")
 
@@ -8,7 +9,7 @@ iniciar = Blueprint('bp_inicio', __name__, static_folder="static", template_fold
 def inicio():
     login = comprobar_sesion()[0]
     if login:
-        return render_template('inicio.html')
+        return render_template('inicio.html', cards=cards)
     else:
         return render_template('ingresar.html')
 
---
+++ lib/cards.py
@@ -0,0 +1,17 @@
+cards = [
+    ['Inventario',
+     'Almacenamiento de las piezas presentes en los talleres.',
+     'También se encuentran aquí las máquinas y sus lista de componentes.',
+     'Visitar el inventario'
+     ],
+    ['Registro de Incidencias',
+     'Almacenamiento de las piezas presentes en los talleres.',
+     'También se encuentran aquí las máquinas y sus lista de componentes.',
+     'Visitar el inventario'
+     ],
+    ['Mantenimeinto preventivo',
+     'Almacenamiento de las piezas presentes en los talleres.',
+     'También se encuentran aquí las máquinas y sus lista de componentes.',
+     'Visitar el inventario'
+     ]
+]
