1fa2dbe3bbfcc5c2e544d03f77259aeb4b2930e0
Brr1-99 <93494400+Brr1-99@users.noreply.github.com>
2022-02-11 10:32:53 +0100

    cards updated

--- lib/cards.py
+++ lib/cards.py
@@ -5,13 +5,13 @@ cards = [
      'Visitar el inventario'
      ],
     ['Registro de Incidencias',
-     'Almacenamiento de las piezas presentes en los talleres.',
-     'También se encuentran aquí las máquinas y sus lista de componentes.',
-     'Visitar el inventario'
+     'Recopliación de las incidencias ocurridas en las diferentes máquinas.',
+     'Contiene información relevante como la ubicación, hora y estado de las distintas incidencias.',
+     'Visitar el Registro de Incidencias'
      ],
-    ['Mantenimeinto preventivo',
-     'Almacenamiento de las piezas presentes en los talleres.',
-     'También se encuentran aquí las máquinas y sus lista de componentes.',
-     'Visitar el inventario'
+    ['Mantenimiento preventivo',
+     'Almacenamiento de las revisiones periódicas a realizar en los talleres.',
+     'Contiene información importante como la máquina y el parámetro a revisar.',
+     'Visitar el Registro de Manteminiento Preventivo'
      ]
 ]
